892747a37692e1a503e7874f8a40f90ebd912ecb
sakalaus <sakalaus@gmail.com>
2022-12-14 18:43:02 +0100

    Animated welcome screen

--- feature-welcome-screen/src/main/java/com/rc/machinerybooker/feature/welcome_screen/screen/WelcomeScreenRoute.kt
+++ feature-welcome-screen/src/main/java/com/rc/machinerybooker/feature/welcome_screen/screen/WelcomeScreenRoute.kt
@@ -1,13 +1,18 @@
 package com.rc.machinerybooker.feature.welcome_screen.screen
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,8 +35,20 @@ fun WelcomeScreenScreen(
     onWelcomeScreenShow: () -> Unit,
     onNavigateToMachineryOrderList: () -> Unit
 ) {
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
     LaunchedEffect(key1 = true) {
-        delay(2000L)
+        scale.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(
+                durationMillis = 2000,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(1000L)
         onWelcomeScreenShow()
         onNavigateToMachineryOrderList()
     }
@@ -42,6 +59,7 @@ fun WelcomeScreenScreen(
         Image(
             modifier = Modifier
                 .fillMaxSize()
+                .scale(scale.value)
                 .padding(horizontal = 36.dp),
             painter = painterResource(welcomeScreenRes.drawable.logo),
             contentDescription = ""
